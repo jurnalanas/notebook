efe4696a04fd83f26e71e18cf3bba614288ada89
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2018-03-27 15:00:27 +0700

    automatic update, see commit logs for detailed info

---
+++ archive-and-publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd "/Users/muhammadnasrurrohman/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/wiki_html"
+git add .
+git commit -m "automatic update, see commit logs for detailed info"
+git checkout gh-pages
+git push origin gh-pages
+# then publish
+cd "/Users/muhammadnasrurrohman/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/wiki_html"
+git checkout master
+cp -a "/Users/muhammadnasrurrohman/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/wiki"/* "/Users/muhammadnasrurrohman/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/wiki_html"
+git add .
+git commit -m "automatic update, see commit logs for detailed info"
+git push origin master
--- archive.sh
+++ archive.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd $IAWRITER/wiki-html
-g co gh-pages
-ggpush
+cd "/Users/muhammadnasrurrohman/Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/wiki_html"
+git checkout gh-pages
+git add .
+git commit -m "automatic update, see commit logs for detailed info"
+git push origin gh-pages
